e3bc75908df90856bcc695614450af213345a8c3
zkstewa <zkstewa2@illinois.edu>
2015-09-06 19:04:12 -0500

    Added Command line args
    Basic command line args added with optparse

--- engine.py
+++ engine.py
@@ -1,18 +1,43 @@
+import optparse
+import time
 import requests
 import datetime
+import random
 from constants import *
 from secrets import access_token, device
-from time import sleep
 domain = "https://api.particle.io/v1/devices/{0}/{1}"
 
+
+#get command line args
+def args(i):
+
+#instantiate parser
+    parser=optparse.OptionParser()
+#Add additional arguments following the structure of the line below
+    parser.add_option('--illini',action="store",dest="i",help="0=illini forward, 1=illini backward, 2=Blue",default="0")
+#send args to variables
+    options, args=parser.parse_args()
+    i=int(options.i)
+    return i
+
 def main():
-  i = 0
+#define globals for args
+  i=0
+#get command line args
+  i=args(i)
+  print "debug i after args()",i
+  clear_strip()
   while True:
-    if i % 2 == 0:
+    colors = [orange, blue, black, orange, blue]
+    if i == 0:
       illinois_forwards()
-    else:
+    if i == 1:
       illinois_backwards()
-    i = i + 1
+      i = i + 1
+    if(i == 2):
+      colors=[blue]
+      light_strip_forwards(colors,30)
+
 
 def illinois_forwards():
   return light_strip_forwards(illinois, 30)
@@ -27,7 +52,7 @@ def clear_strip():
   return send_request([black], "color")
 
 def light_strip_forwards(colors, delay):
-    return send_request([str(delay)] + colors, "forwards")  
+    return send_request([str(delay)] + colors, "forwards")
 
 def light_strip_backwards(colors, delay):
     return send_request([str(delay)] + colors, "backwards")
